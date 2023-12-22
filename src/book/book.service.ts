1cabef61f0d3436d33631ba175ab7f3315d92977
Shabab007 <shabab.23rhythm@gmail.com>
2023-12-22 17:11:57 +0600

    pagination and serach

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schema/book.schema';
 import * as mongoose from 'mongoose';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
 export class BookService {
@@ -9,8 +10,22 @@ export class BookService {
     @InjectModel(Book.name)
     private bookModel: mongoose.Model<Book>,
   ) {}
-  async findAll(): Promise<Book[]> {
-    const books = await this.bookModel.find();
+  async findAll(query: Query): Promise<Book[]> {
+    const showPerPage = 2;
+    const currentPage = Number(query.page) || 1;
+    const skip = showPerPage * (currentPage - 1);
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+    const books = await this.bookModel
+      .find({ ...keyword })
+      .limit(showPerPage)
+      .skip(skip);
     return books;
   }
   create = async (book: Book): Promise<Book> => {
